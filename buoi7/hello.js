dec0cade48f8ea5b1245cea8e6b7ce0c0ab27d2c
Duc <d.staffeld@yahoo.de>
2025-09-20 16:56:26 +0700

    deleted:    README.md 	new file:   buoi7/hello.js 	new file:   buoi7/jslearning.html

---
+++ buoi7/hello.js
@@ -0,0 +1,10 @@
+let a = 2;
+let b = 3;
+
+console.log("a + b =", a + b); // sum
+console.log("a - b =", a - b); // difference
+console.log("a * b =", a * b); // product
+console.log("a / b =", a / b); // quotient
+console.log("a % b =", a % b); // remainder
+//bai2
+console.log("a^b =", a ** b); // exponentiation
